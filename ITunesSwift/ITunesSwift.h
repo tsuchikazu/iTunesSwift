3c4506bbc098fe7d6fb5758864c8a3b741ca755f
tsuchikazu <huneyhunt77@gmail.com>
2014-09-21 00:23:10 +0900

    Initial Commit

---
+++ ITunesSwift/ITunesSwift.h
@@ -0,0 +1,19 @@
+//
+//  ITunesSwift.h
+//  ITunesSwift
+//
+//  Created by 土屋 和良 on 2014/09/20.
+//  Copyright (c) 2014年 tsuchikazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ITunesSwift.
+FOUNDATION_EXPORT double ITunesSwiftVersionNumber;
+
+//! Project version string for ITunesSwift.
+FOUNDATION_EXPORT const unsigned char ITunesSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITunesSwift/PublicHeader.h>
+
+
